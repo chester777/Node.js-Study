916c54f1d6aab7c85e76fcbc0cb04ce44f125cdf
chester777 <aster9797@gmail.com>
2016-07-26 21:47:38 +0900

    init & winston, mochar

---
+++ mocha/mocha_bdd_test.js
@@ -0,0 +1,20 @@
+var assert = require("assert");
+var Atm = require("./atm");
+var atm = new Atm(5000);
+
+describe('Feature : get cash from an ATM', function() {
+	context('Scenario : success', function() {
+		describe('When the user asks the ATM for 500', function() {
+			assert.equal(atm.withDraw(500));
+
+			it('Then the ATM will have 500', function() {
+				assert.equal(atm.AtmRemainingCash(), 500);
+			});
+
+			it("Then the user's account will have 4500", function(done) {
+				assert.equal(atm.getAccount(), 4500);
+				done();
+			});
+		});
+	});
+});
---
+++ winston/winston.js
@@ -0,0 +1,40 @@
+var winston = require('winston')
+var Logger = winston.Logger
+var File = winston.transports.File
+var Console = winston.transports.Console
+
+var options = {
+
+	levels: {
+		info: 0,
+		normal: 1,
+		minor: 2,
+		critical: 3
+	},
+
+	colors: {
+		info: 'green',
+		normal: 'blue',
+		minor: 'yellow',
+		critical: 'red'
+	},
+
+	transports: [
+		new File({
+			name: 'log-file',
+			filename: 'log-file.log',
+			level: 'info'
+		}),
+
+		new Console({
+			level: 'critical',
+			colorize: true
+		})
+	]
+}
+
+var logger = new Logger(options)
+logger.info('info!!');
+logger.normal('normal!!');
+logger.minor('minor!!');
+logger.critical('critical!!');
